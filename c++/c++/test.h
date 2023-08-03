60912bdad2ec0184a9c20c781cd1890206409cd3
Shadows <126090020+chenran2022@users.noreply.github.com>
2023-08-03 15:40:46 +0800

    Update test.h

--- c++/c++/test.h
+++ c++/c++/test.h
@@ -1,1 +1,35 @@
 #pragma once
+
+//类的声明
+//class Point
+//{
+//public:
+//
+//	void SetPoint(int x, int y);
+//	
+//	int GetPoint_x();
+//	
+//	int GetPoint_y();
+//
+//private:
+//	int p_x;
+//	int p_y;
+//};
+//
+//class Circle
+//{
+//public:
+//
+//	void SetCircle_r(int r);
+//	
+//	int GetCircle_r();
+//	
+//	void SetCenter(Point Center);
+//	
+//	Point GetCenter();
+//	
+//private:
+//	int m_r;
+//	Point m_Center;//圆心
+//};
+//void P_C(Point& p, Circle& c);
