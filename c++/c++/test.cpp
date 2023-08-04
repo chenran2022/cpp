b1883cae16c8ee5ee01c5d3e660ac78eedd37f80
Shadows <126090020+chenran2022@users.noreply.github.com>
2023-08-04 10:32:14 +0800

    Update test.cpp

--- c++/c++/test.cpp
+++ c++/c++/test.cpp
@@ -459,3 +459,247 @@ using namespace std;
 //	system("pause");
 //	return 0;
 //}
+
+//构造函数的分类和调用
+//分类：
+//1：按参数分类：有参构造和无参构造（默认构造）
+//2: 按照类型分类：普通构造函数和拷贝构造函数
+
+//class Person
+//{
+//public:
+//	Person()
+//	{
+//		cout << "无参构造" << endl;
+//	}
+//	Person(int a)
+//	{
+//		age = a;
+//		cout << "有参构造" << endl;
+//	}
+//	Person(const Person& p) 
+//	//拷贝构造函数：把p的属性拷贝到当前对象身上，const是为了不修改p
+//	{
+//		age = p.age;
+//		cout << "拷贝构造" << endl;
+//	}
+//	~Person()
+//	{
+//		cout << "析构函数" << endl;
+//	}
+//
+//	int age;
+//};
+//
+////调用：
+//void test1()
+//{
+//	//1：括号法
+//	Person p1;//默认构造函数调用
+//	//注意：使用默认构造函数调用不能加()
+//	//		Person p1();因为编译器会认为这是一个声明
+//	Person p2(10);//有参构造函数调用
+//	Person p3(p2);//拷贝函数调用
+//	cout << p2.age << endl;
+//	cout << p3.age << endl;
+//
+//	//2：显示法
+//	Person p4;
+//	Person p5 = Person(20);//有参构造函数调用
+//	Person p6 = Person(p5);//拷贝函数调用
+//	Person(10);//匿名对象：当前行执行后，系统会立即回收掉匿名对象
+//	cout << "test匿名对象" << endl;
+//	//注意：不能利用拷贝构造函数初始化匿名对象
+//	//		Person(p5);编译器会认为 Person(p5)===Person p5;
+//	
+//	//3:隐式转换法
+//	Person p6 = 30;//相当于Person p6 = Person (30);
+//	Person p7 = p6;//相当于Person p7 = Person (p6);
+//}
+//int main()
+//{
+//	test1();
+//	system("pause");
+//	return 0;
+//}
+
+
+//拷贝构造函数调用时机
+//1：使用一个已经创建完毕的对象来初始化一个新对象
+//2：值传递的方式给函数参数传值
+//3：以值方式返回局部对象
+
+//class Person
+//{
+//public:
+//	Person()
+//	{
+//		cout << "无参构造" << endl;
+//	}
+//	Person(int a)
+//	{
+//		age = a;
+//		cout << "有参构造" << endl;
+//	}
+//	Person(const Person& p) 
+//	{
+//		age = p.age;
+//		cout << "拷贝构造" << endl;
+//	}
+//	~Person()
+//	{
+//		cout << "析构函数" << endl;
+//	}
+//
+//	int age;
+//};
+//
+////1：使用一个已经创建完毕的对象来初始化一个新对象
+//void test1()
+//{
+//	Person p1(10);
+//	Person p2(p1);
+//	cout << p2.age << endl;
+//}
+//
+////2：值传递的方式给函数参数传值
+//void doWork(Person p)  //相当于隐式转换法：Person p = p 
+//{
+//
+//}
+//void test2()
+//{
+//	Person p;
+//	doWork(p);
+//}
+//
+////3：以值方式返回局部对象
+//Person doWork2()
+//{
+//	Person p1;
+//	cout << (int*)&p1 << endl;
+//	return  Person(p1);
+//}
+//void test3()
+//{
+//	Person p = doWork2();
+//	cout << (int*)&p << endl;
+//}
+//int main()
+//{
+//	//test1();
+//	//test2();
+//	test3();
+//	return 0;
+//}
+
+
+//构造函数调用规则：
+// 默认构造函数（无参，函数体为空）
+// 默认析构函数（无参，函数体为空）
+// 默认拷贝构造函数，对属性进行值拷贝（浅拷贝）
+// 
+// 创建一个了类，C++编译器会给每一个类都添加至少三个函数：默认构造、析构函数、拷贝构造
+// 
+//如果用户定义有参构造函数，C++不再提供默认无参构造，但会提供默认拷贝构造
+//如果用户定义拷贝构造函数，C++不再提供其他构造函数
+
+
+//深拷贝和浅拷贝
+//浅拷贝：简单的赋值拷贝操作
+//深拷贝：在堆区重新申请空间，进行拷贝操作
+//默认拷贝函数就是浅拷贝
+
+//class Person
+//{
+//public:
+//	Person()
+//	{
+//		cout << "无参构造" << endl;
+//	}
+//	Person(int a,int h)
+//	{
+//		age = a;
+//		Height = new int(h);
+//		cout << "有参构造" << endl;
+//	}
+//	/*Person(const Person& p)
+//	{
+//		age=p.age;
+//		Height=p.Height;
+//		cout << "浅拷贝构造" << endl;
+//	}*/
+//	Person(const Person& p)
+//	{
+//		age = p.age;
+//		Height = new int(*p.Height);
+//		cout << "深拷贝构造" << endl;
+//	}
+//	~Person()
+//	{
+//		//析构代码：将堆区的空间进行释放
+//		//注意：拷贝构造函数里的浅拷贝会造成堆区的内存重复释放，造成非法操作，所以要进行深拷贝
+//		if (Height != NULL)
+//		{
+//			delete Height;
+//			Height = NULL;
+//		}
+//		cout << "析构函数" << endl;
+//	}
+//	int age;
+//	int* Height;
+//};
+//void test()
+//{
+//	Person p1(18,175);
+//	cout << p1.age << "  " << *p1.Height << endl;
+//	Person p2(p1);
+//	cout << p2.age << "  " << *p2.Height << endl;
+//}
+//int main()
+//{
+//	test();
+//	return 0;
+//}
+
+
+//初始化列表 - 用来初始化属性
+//语法   -  构造函数():属性一(值1),属性二(值2),...{}
+//1.常量成员，因为常量只能初始化不能赋值，所以必须放在初始化列表里面
+//2.引用类型，引用必须在定义的时候初始化，并且不能重新赋值，所以也要写在初始化列表里面
+//3. 没有默认构造函数的类类型，因为使用初始化列表可以不必调用默认构造函数来初始化，而是直接调用拷贝构造函数初始化
+//class Person
+//{
+//public:
+//	/*Person(int a, int b, int c)
+//	{
+//		m_a = a;
+//		m_b = b;
+//		m_c = c;
+//	}*/
+//	/*Person() :m_a(10), m_b(20), m_c(30)
+//	{
+//
+//	}*/
+//	Person(int a,int b,int c) :m_a(a), m_b(b), m_c(c)
+//	{
+//
+//	}
+//	int m_a;
+//	int m_b;
+//	int m_c;
+//};
+//void test()
+//{
+//	/*Person p(10, 20, 30);
+//	cout << p.m_a << "  " << p.m_b << "  " << p.m_c << endl;*/
+//	/*Person p1;
+//	cout << p1.m_a << "  " << p1.m_b << "  " << p1.m_c << endl;*/
+//	Person p2(10, 20, 30);
+//	cout << p2.m_a << "  " << p2.m_b << "  " << p2.m_c << endl;
+//}
+//int main()
+//{
+//	test();
+//	return 0;
+//}
