0a22e41a7c934f46bc48f110dd53f3f1003a354f
Shadows <126090020+chenran2022@users.noreply.github.com>
2024-08-17 22:13:18 +0800

    Update 继承.cpp

--- c++/c++/继承.cpp
+++ c++/c++/继承.cpp
@@ -95,7 +95,7 @@ int main()
 	sobj = pobj;
 	//3.基类的指针可以通过强制类型转换赋值给派生类的指针
 	ptr = &sobj;
-	Student * ps1 = (Student*)ptr; // 这种情况转换时可以的。
+	Student* ps1 = (Student*)ptr; // 这种情况转换时可以的。
 	ps1->_No = 10;
 	ptr = &pobj;
 	Student* ps2 = (Student*)ptr; // 这种情况转换时虽然可以，但是会存在越界访问的问题
@@ -139,6 +139,7 @@ int main()
 }
 */
 
+/*
 class A
 {
 public:
@@ -164,4 +165,306 @@ int main()
 	b.A::fun();
 	b.fun(1);
 	return 0;
+}*/
+
+
+//派生类的默认成员函数
+
+/*
+class Person
+{
+public: 
+	Person()
+	{
+		cout << "Person()" << endl;
+	}
+	~Person()
+	{
+		cout << "~Person()" << endl;		
+	}
+};
+class Student :public Person
+{
+
+};
+int main()
+{
+	Student stu; 
+	//子类继承父类的成员部分是由父类进行初始化构造，清理是由父类析构清理
+	return 0;
 }
+*/
+
+/*
+class Person
+{
+public:
+	Person(const char* name = "peter")
+		: _name(name)
+	{
+		cout << "Person()" << endl;
+	}
+	Person(const Person& p)
+		: _name(p._name)
+	{
+			cout << "Person(const Person& p)" << endl;
+	}
+	Person& operator=(const Person& p)
+	{
+		cout << "Person operator=(const Person& p)" << endl;
+		if (this != &p)
+			_name = p._name;
+		return *this;
+	}
+	~Person()
+	{
+		cout << "~Person()" << endl;
+	}
+protected:
+	string _name;
+};
+class Student :public Person
+{
+public:
+	//Student(const char* str,const int i) :_name(str),_stuid(i)
+	//{
+	//}  
+	//子类不能直接初始化父类的成员变量，父类的部分必须由父类初始化构造，可以使用Person(str)进行初始化构造，不写的话就调用父类的默认构造函数
+	Student(const char* str="wow", const int i=18) :Person(str), _stuid(i)
+	{
+		cout << "Student(const char* str, const int i)" << endl;
+	}
+
+	//Student(const Student& stu) :_name(stu._name), _stuid(stu._stuid)
+	//{
+	//}
+	//拷贝构造，父类的成员部分调用父类的拷贝构造函数
+	Student(const Student& stu) :Person(stu), _stuid(stu._stuid)//子类对象赋值给父类，就是切割
+	{
+		cout << "Student(const Student& stu)" << endl;
+	}
+
+	Student& operator=(const Student& s)
+	{
+		cout << "Student& operator=(const Student& s)" << endl;
+		if (this != &s)
+		{
+			_stuid = s._stuid;
+			//this->operator=(s);
+			// 与父类的operator=函数名相同，被隐藏了，不能直接调用
+			Person::operator=(s);
+		}
+		return *this;
+	}
+	~Student()//子类的析构函数和父类的析构函数构成隐藏，因为他们的名字会被编译器统一处理成destructor（跟多态有关）
+	{
+		//~Person();//不能直接调用父类的析构函数，因为是隐藏
+		//Person::~Person();//正确用法不需要显式调用，结束会自动调用父类析构函数
+		//实际上也不能显示调用调用父类的析构函数，因为子类析构函数结束后会自动调用父类析构函数，这样才能保证先析构子类，再析构父类
+		cout << "~Student()" << endl;
+	}
+protected:
+	int _stuid;
+};	
+int main()
+{
+	Student stu("chenran",2022);
+	Student s2(stu);  //拷贝构造，父类的成员部分调用父类的拷贝构造函数
+
+	Student s3 ;
+	s3 = s2;
+	return 0;
+}
+*/
+//ps:如何设计一个不能被继承的类？把构造函数放在private里，这样子类就无法调用父类构造函数
+
+
+//友元关系不能继承，也就是说基类友元不能访问子类私有和保护成员
+/*class Student;
+class Person
+{
+public:
+	friend void Display(const Person& p, const Student& s);
+protected:
+	string _name; // 姓名
+};
+class Student : public Person
+{
+protected:
+	int _stuNum; // 学号
+};
+void Display(const Person& p, const Student& s)
+{
+	cout << p._name << endl;
+	cout << s._stuNum << endl;
+	//父类友元函数不会被继承，所以访问不了子类私有和保护成员
+	//如果想访问，就在子类里再写一个友元函数
+}
+void main()
+{
+	Person p;
+	Student s;
+	Display(p, s);
+}*/
+
+
+//基类定义了static静态成员，则整个继承体系里面只有一个这样的成员。
+//无论派生出多少个子类，都只有一个static成员实例。
+/*class Person
+{
+public:
+	Person() { ++_count; }
+	string _name; // 姓名
+
+	static int _count; // 统计人的个数。
+};
+int Person::_count = 0;
+class Student : public Person
+{
+protected:
+	int _stuNum; // 学号
+};
+int main()
+{
+	Person p;
+	Student s;
+	p._name = "www";
+	s._name = "hhh";
+
+	p._count = 1;
+	s._count = 2;
+	Person::_count++;
+	cout << p._count << "  " << s._count << endl;
+	return 0;
+}*/
+
+
+//多继承：一个子类有两个或以上直接父类时称这个继承关系为多继承
+//菱形继承：菱形继承是多继承的一种特殊情况。
+//菱形继承的问题：从下面的对象成员模型构造，可以看出菱形继承有数据冗余和二义性的问题。
+//在Assistant的对象中Person成员会有两份。
+/*class Person
+{
+public:
+	string _name; // 姓名
+};
+class Student : public Person
+{
+protected:
+	int _num; //学号
+};
+class Teacher : public Person
+{
+protected:
+	int _id; // 职工编号
+};
+class Assistant : public Student, public Teacher
+{
+protected:
+	string _majorCourse; // 主修课程
+};
+void main()
+{
+	// 这样会有二义性无法明确知道访问的是哪一个
+	Assistant a;
+	a._name = "peter";
+	// 需要显示指定访问哪个父类的成员可以解决二义性问题，但是数据冗余问题无法解决
+	a.Student::_name = "xxx";
+	a.Teacher::_name = "yyy";
+}*/
+//虚拟继承可以解决菱形继承的二义性和数据冗余的问题。
+//如上面的继承关系，在Student和Teacher的继承Person时使用虚拟继承，即可解决问题。
+//需要注意的是，虚拟继承不要在其他地方去使用。
+/*class Person
+{
+public:
+	string _name; // 姓名
+};
+class Student : virtual public Person
+{
+protected:
+	int _num; //学号
+};
+class Teacher : virtual public Person
+{
+protected:
+	int _id; // 职工编号
+};
+class Assistant : public Student, public Teacher
+{
+protected:
+	string _majorCourse; // 主修课程
+};
+void main()
+{
+	Assistant a;
+	a._name = "peter";
+}*/
+
+/*
+class A
+{
+public:
+	int _a;
+};
+ //class B : public A
+class B : virtual public A
+{
+public:
+	int _b;
+};
+ //class C : public A
+class C : virtual public A
+{
+public:
+	int _c;
+};
+class D : public B, public C
+{
+public:
+	int _d;
+};
+int main()
+{
+	cout << sizeof(D) << endl; //不用virtual时是20，使用virtual是24
+	D d;
+	d.B::_a = 1;
+	d.C::_a = 2;
+	d._b = 3;
+	d._c = 4;
+	d._d = 5;
+	d._a = 3;  //继承的相同类共用一块空间，B、C的空间内存放着到相同类地址的偏移量的虚基表的地址
+	return 0;
+}*/
+
+
+//继承与组合：
+// public继承是一种is-a的关系。也就是说每个派生类对象都是一个基类对象。
+//组合是一种has-a的关系。假设B组合了A，每个B对象中都有一个A对象。
+// 优先使用对象组合，而不是类继承
+// Car和BMW Car和Benz构成is-a的关系
+class Car {
+protected:
+	string _colour = "白色"; // 颜色
+	string _num = "陕ABIT00"; // 车牌号
+};
+class BMW : public Car {
+public:
+	void Drive() { cout << "好开-操控" << endl; }
+};
+class Benz : public Car {
+public:
+	void Drive() { cout << "好坐-舒适" << endl; }
+};
+// Tire和Car构成has-a的关系
+class Tire {
+protected:
+	string _brand = "Michelin"; // 品牌
+	size_t _size = 17; // 尺寸
+};
+class Car {
+protected:
+	string _colour = "白色"; // 颜色
+	string _num = "陕ABIT00"; // 车牌号
+	Tire _t; // 轮胎
+};
